9d1ed7572e25f319ac8ce78ee6061ae6c80ef1a8
Tommaso Parigi <tommaso.parigi06@gmail.com>
2024-09-20 10:35:06 +0200

    Esercizio terminato

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Tasso d'interesse</h1>
+    <?php
+        $capitale  = 20000;
+        $tasso = 0.10; // percentuale
+        $giorni = 30;
+        $interesse = ($capitale*$tasso*$giorni)/36500;
+        echo "<h4>Capitale: $capitale</h4>";
+        echo "<h4>Tasso: ". $tasso*100 ."%</h4>";
+        echo "<h4>Giorni: $giorni</h4>";
+        echo "<h2 style='color:green'>L'interesse calcolato è di $interesse</h2>";
+    ?>
+</body>
+</html>
